9faa1371ab67c758c36adf230f17ea57eb9805fa
Ildar Musin <zildermann@gmail.com>
2019-04-01 14:59:46 +0200

    Travis fix

--- install_arrow.sh
+++ install_arrow.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 sudo apt install -y -V apt-transport-https lsb-release
-sudo wget -O /usr/share/keyrings/apache-arrow-keyring.gpg https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
+curl https://dist.apache.org/repos/dist/dev/arrow/KEYS | sudo apt-key add -
 sudo tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
-deb [arch=amd64 signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
-deb-src [signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+deb [arch=amd64] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+deb-src https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
 APT_LINE
-sudo apt update --allow-insecure-repositories
+sudo apt update --allow-insecure-repositories --allow-unauthenticated
+sudo apt install -y -V --allow-unauthenticated apache-arrow
 sudo apt install -y -V --allow-unauthenticated libarrow-dev
+sudo apt install -y -V --allow-unauthenticated libarrow-dev_0.13
 sudo apt install -y -V --allow-unauthenticated libparquet-dev
